dd7cb5ead0e75ac6587efe83352da0099c5b94ba
Andrew Shumway <shumway.andrew@yahoo.com>
2021-09-28 10:33:30 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Andrew Shumway";
+    int age = 28;
+    String state = "Texas";
+
+    System.out.print("Name: " + name);
+    System.out.print(" Age: " + age);
+    System.out.println(" State: " + state);
+
+  }
+}

01a944c6ed4f20f9a1fe59dffc493c1326876d44
Andrew Shumway <shumway.andrew@yahoo.com>
2021-09-29 13:18:59 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1-10
+    for (int n = 1; n <= 10; n++) {
+      System.out.println(n);
+    }
+    //TODO Write another for loop that will print the numbers 10-1.
+    for (int n = 10; n >= 1; n--) {
+      System.out.println(n);
+    }
+  }
+}
